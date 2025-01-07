fec3ce4d41a6c8fb888495f858463afe1fe458b4
ricardo-ervilha <ricardoervilha456@gmail.com>
2025-01-07 09:44:12 -0300

    checkpoint

--- trabalho-1/c/gsrb.c
+++ trabalho-1/c/gsrb.c
@@ -35,73 +35,72 @@ int main(int argc, char* argv[]){
     start = omp_get_wtime();
     while(error > tol){
         diff = 0;
-        #pragma omp parallel num_threads(NUM_THREADS) private(tmp, i, j) reduction(+:diff)
+        #pragma omp parallel num_threads(NUM_THREADS) private(tmp, i, j, j_p, j_m, i_p, i_m, k_zp, k_zm, k_xp, k_xm, val1, val2, val3, val4) reduction(+:diff)
         {
             #pragma omp for
             for(j = 0; j < tamz; j++){
                 for(i = (j % 2 == 0 ? 1 : 2); i < tamx; i += 2){
-                    // if((i+j)%2 == 1){
-                        tmp = u_new[j][i];
+                    
+                    tmp = u_new[j][i];
 
-                        j_p = (j == tamz - 1) ? (j - 1) : (j + 1);
-                        j_m = (j == 0) ? (j + 1) : (j - 1);
-                        i_p = (i == tamx - 1) ? (i - 1) : (i + 1);
-                        i_m = i - 1;
+                    j_p = (j == tamz - 1) ? (j - 1) : (j + 1);
+                    j_m = (j == 0) ? (j + 1) : (j - 1);
+                    i_p = (i == tamx - 1) ? (i - 1) : (i + 1);
+                    i_m = i - 1;
 
-                        k_zp = k_harm(k(x[i], z[j]), k(x[i], z[j_p]));
-                        k_zm = k_harm(k(x[i], z[j]), k(x[i], z[j_m]));
-                        k_xp = k_harm(k(x[i], z[j]), k(x[i_p], z[j]));
-                        k_xm = k_harm(k(x[i], z[j]), k(x[i_m], z[j]));
+                    k_zp = k_harm(k(x[i], z[j]), k(x[i], z[j_p]));
+                    k_zm = k_harm(k(x[i], z[j]), k(x[i], z[j_m]));
+                    k_xp = k_harm(k(x[i], z[j]), k(x[i_p], z[j]));
+                    k_xm = k_harm(k(x[i], z[j]), k(x[i_m], z[j]));
 
-                        val1 = (k_zp * u_new[j_p][i] + k_zm * u_new[j_m][i] + k_xp * u_new[j][i_p] + k_xm * u_new[j][i_m]) / (h*h);
+                    val1 = (k_zp * u_new[j_p][i] + k_zm * u_new[j_m][i] + k_xp * u_new[j][i_p] + k_xm * u_new[j][i_m]) / (h*h);
 
-                        val2 = omega_b(x[i], z[j], u_new[j][i]) * c_b(x[i], z[j]) * T_a + Q_m(x[i], z[j]) + Q_r(x[i], z[j]);
-                        
-                        val3 = k_zp / (h*h) + k_zm / (h*h) + k_xp / (h*h) + k_xm / (h*h);
-                        
-                        val4 = omega_b(x[i], z[j], u_new[j][i]) * c_b(x[i], z[j]);
-                        
-                        u_new[j][i] = (val1 + val2) / (val3 + val4);   
+                    val2 = omega_b(x[i], z[j], u_new[j][i]) * c_b(x[i], z[j]) * T_a + Q_m(x[i], z[j]) + Q_r(x[i], z[j]);
+                    
+                    val3 = k_zp / (h*h) + k_zm / (h*h) + k_xp / (h*h) + k_xm / (h*h);
+                    
+                    val4 = omega_b(x[i], z[j], u_new[j][i]) * c_b(x[i], z[j]);
+                    
+                    u_new[j][i] = (val1 + val2) / (val3 + val4);   
 
-                        diff += fabs(u_new[j][i] - tmp); 
+                    diff += fabs(u_new[j][i] - tmp); 
 
-                    // }
                 }
             }  
             #pragma omp barrier
             
         }
 
-        #pragma omp parallel num_threads(NUM_THREADS) private(tmp, i, j) reduction(+:diff)
+        #pragma omp parallel num_threads(NUM_THREADS) private(tmp, i, j, j_p, j_m, i_p, i_m, k_zp, k_zm, k_xp, k_xm, val1, val2, val3, val4) reduction(+:diff)
         {
             #pragma omp for
             for(j = 0; j < tamz; j++){
                 for(i = (j % 2 == 0 ? 2 : 1); i < tamx; i += 2){
-                    // if((i+j)%2 == 0){
-                        tmp = u_new[j][i];
+                    
+                    tmp = u_new[j][i];
 
-                        j_p = (j == tamz - 1) ? (j - 1) : (j + 1);
-                        j_m = (j == 0) ? (j + 1) : (j - 1);
-                        i_p = (i == tamx - 1) ? (i - 1) : (i + 1);
-                        i_m = i - 1;
+                    j_p = (j == tamz - 1) ? (j - 1) : (j + 1);
+                    j_m = (j == 0) ? (j + 1) : (j - 1);
+                    i_p = (i == tamx - 1) ? (i - 1) : (i + 1);
+                    i_m = i - 1;
 
-                        k_zp = k_harm(k(x[i], z[j]), k(x[i], z[j_p]));
-                        k_zm = k_harm(k(x[i], z[j]), k(x[i], z[j_m]));
-                        k_xp = k_harm(k(x[i], z[j]), k(x[i_p], z[j]));
-                        k_xm = k_harm(k(x[i], z[j]), k(x[i_m], z[j]));
+                    k_zp = k_harm(k(x[i], z[j]), k(x[i], z[j_p]));
+                    k_zm = k_harm(k(x[i], z[j]), k(x[i], z[j_m]));
+                    k_xp = k_harm(k(x[i], z[j]), k(x[i_p], z[j]));
+                    k_xm = k_harm(k(x[i], z[j]), k(x[i_m], z[j]));
 
-                        val1 = (k_zp * u_new[j_p][i] + k_zm * u_new[j_m][i] + k_xp * u_new[j][i_p] + k_xm * u_new[j][i_m]) / (h*h);
+                    val1 = (k_zp * u_new[j_p][i] + k_zm * u_new[j_m][i] + k_xp * u_new[j][i_p] + k_xm * u_new[j][i_m]) / (h*h);
 
-                        val2 = omega_b(x[i], z[j], u_new[j][i]) * c_b(x[i], z[j]) * T_a + Q_m(x[i], z[j]) + Q_r(x[i], z[j]);
-                        
-                        val3 = k_zp / (h*h) + k_zm / (h*h) + k_xp / (h*h) + k_xm / (h*h);
-                        
-                        val4 = omega_b(x[i], z[j], u_new[j][i]) * c_b(x[i], z[j]);
-                        
-                        u_new[j][i] = (val1 + val2) / (val3 + val4);   
+                    val2 = omega_b(x[i], z[j], u_new[j][i]) * c_b(x[i], z[j]) * T_a + Q_m(x[i], z[j]) + Q_r(x[i], z[j]);
+                    
+                    val3 = k_zp / (h*h) + k_zm / (h*h) + k_xp / (h*h) + k_xm / (h*h);
+                    
+                    val4 = omega_b(x[i], z[j], u_new[j][i]) * c_b(x[i], z[j]);
+                    
+                    u_new[j][i] = (val1 + val2) / (val3 + val4);   
 
-                        diff += fabs(u_new[j][i] - tmp); 
-                    // }
+                    diff += fabs(u_new[j][i] - tmp); 
+                    
                 }
             } 
             #pragma omp barrier 
